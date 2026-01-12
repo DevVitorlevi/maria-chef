e7a78afff7beded0f25f8ebe02f78aef5c9e2bfb
DevVitorlevi <levivitor8@gmail.com>
2026-01-12 03:23:06 -0300

    feat(controller): add create dish controller

---
+++ backend/src/http/controllers/dish/create.ts
@@ -0,0 +1,37 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+import { CategoriaPrato, CategoriaIngrediente } from "@/generated/prisma/enums";
+import { makeCreateDishUseCase } from "@/use-cases/factories/make-create-dish-use-case";
+
+export async function create(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const dishBodySchema = z.object({
+    nome: z.string().min(1),
+    categoria: z.nativeEnum(CategoriaPrato),
+    ingredientes: z.array(
+      z.object({
+        nome: z.string().min(1),
+        quantidade: z.number().positive(),
+        unidade: z.string().min(1),
+        categoria: z.nativeEnum(CategoriaIngrediente),
+      })
+    ).min(1),
+  });
+
+  const { nome, categoria, ingredientes } = dishBodySchema.parse(request.body);
+
+  const createDishUseCase = makeCreateDishUseCase();
+
+  const { prato } = await createDishUseCase.execute({
+    nome,
+    categoria,
+    ingredientes,
+  });
+
+  return reply.status(201).send(
+    prato,
+  );
+}
