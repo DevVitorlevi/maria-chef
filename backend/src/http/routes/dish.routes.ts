0bc21696c6cf28faccb70e03e47c3fbb92e45941
DevVitorlevi <levivitor8@gmail.com>
2026-01-13 19:23:00 -0300

    feat(routes): add route to find by id dish

--- backend/src/http/routes/dish.routes.ts
+++ backend/src/http/routes/dish.routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/dish/create";
 import { findAll } from "../controllers/dish/findAll";
+import { findById } from "../controllers/dish/findById";
 
 export function dishRoutes(app: FastifyInstance) {
   app.post("/dish", create)
   app.get("/dish", findAll)
+  app.get("/dish/:id", findById)
 }
