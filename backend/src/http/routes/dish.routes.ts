77f237d07f8d98616d22b58e61a842d94a58917a
DevVitorlevi <levivitor8@gmail.com>
2026-01-13 00:26:21 -0300

    feat(routes): add route to find all dishes

--- backend/src/http/routes/dish.routes.ts
+++ backend/src/http/routes/dish.routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/dish/create";
+import { findAll } from "../controllers/dish/findAll";
 
 export function dishRoutes(app: FastifyInstance) {
   app.post("/dish", create)
+  app.get("/dish", findAll)
 }
