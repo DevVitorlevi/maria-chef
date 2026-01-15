2c15759cfe71ebc0b5456e879632ccf81aed2625
DevVitorlevi <levivitor8@gmail.com>
2026-01-14 22:06:02 -0300

    feat(routes): add route to update dish

--- backend/src/http/routes/dish.routes.ts
+++ backend/src/http/routes/dish.routes.ts
@@ -2,9 +2,11 @@ import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/dish/create";
 import { findAll } from "../controllers/dish/findAll";
 import { findById } from "../controllers/dish/findById";
+import { update } from "../controllers/dish/update";
 
 export function dishRoutes(app: FastifyInstance) {
   app.post("/dish", create)
   app.get("/dish", findAll)
   app.get("/dish/:id", findById)
+  app.put("/dish/:id", update)
 }
