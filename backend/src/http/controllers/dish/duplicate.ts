c518295e416ccc3346c22bebe7d79d830fa1385a
DevVitorlevi <levivitor8@gmail.com>
2026-01-15 22:29:16 -0300

    feat(controller): add duplicate dish controller

---
+++ backend/src/http/controllers/dish/duplicate.ts
@@ -0,0 +1,41 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+import { makeDuplicateDishUseCase } from "@/use-cases/factories/make-duplicate-dish-use-case";
+
+export async function duplicate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const duplicateDishParamsSchema = z.object({
+    dishId: z.string().uuid({ message: "ID inválido" }),
+  });
+
+  const { dishId } = duplicateDishParamsSchema.parse(request.params);
+
+  try {
+    const duplicateDishUseCase = makeDuplicateDishUseCase();
+
+    const { dish } = await duplicateDishUseCase.execute({
+      dishId,
+    });
+
+    return reply.status(201).send(dish);
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.message === "Prato não encontrado") {
+        return reply.status(404).send({
+          message: error.message,
+        });
+      }
+
+      if (error.message === "ID inválido") {
+        return reply.status(400).send({
+          message: error.message,
+        });
+      }
+    }
+
+    throw error;
+  }
+} 
