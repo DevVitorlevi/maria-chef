b09195c744b24f38cf05b3431f809f8c0002a10b
DevVitorlevi <levivitor8@gmail.com>
2026-01-17 14:34:10 -0300

    feat(repository): add delete method in ingredient repository

---
+++ backend/src/repositories/ingredient-repository.ts
@@ -0,0 +1,11 @@
+import { Prisma, type CategoriaIngrediente, type Ingrediente } from "@/generated/prisma/client"
+
+export interface CreateIngredientDTO {
+  nome: string
+  quantidade: number
+  unidade: string
+  categoria: CategoriaIngrediente
+}
+export interface IngredientRepository {
+  create(dishId: string, ingredient: CreateIngredientDTO): Promise<Ingrediente>
+}
