17e2ea92209552497530d4b6831abf5de5be9aec
DevVitorlevi <levivitor8@gmail.com>
2026-01-18 15:29:07 -0300

    feat(routes): add route to update ingredient

--- backend/src/http/routes/ingredient.routes.ts
+++ backend/src/http/routes/ingredient.routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/ingredient/create";
+import { update } from "../controllers/ingredient/update";
 
 export function ingredientroutes(app: FastifyInstance) {
   app.post("/dish/:dishId/ingredient", create)
+  app.put("/dish/:dishId/ingredient/:ingredientId", update)
 }
