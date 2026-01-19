96c81fc6af0c3f7c4ed85f86f27d89a62350efea
DevVitorlevi <levivitor8@gmail.com>
2026-01-19 02:39:21 -0300

    feat(routes): add route to delete ingredient

--- backend/src/http/routes/ingredient.routes.ts
+++ backend/src/http/routes/ingredient.routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/ingredient/create";
+import { deleteIngredient } from "../controllers/ingredient/delete";
 import { update } from "../controllers/ingredient/update";
 
 export function ingredientroutes(app: FastifyInstance) {
   app.post("/dish/:dishId/ingredient", create)
   app.put("/dish/:dishId/ingredient/:ingredientId", update)
+  app.delete("/dish/:dishId/ingredient/:ingredientId/delete", deleteIngredient)
 }
