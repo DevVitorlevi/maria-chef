a72fa21b2fe28a87d48ce5aa88324e4ecee6d3f9
DevVitorlevi <levivitor8@gmail.com>
2026-01-20 16:26:39 -0300

    fix(routes): change route

--- backend/src/http/routes/dish.routes.ts
+++ backend/src/http/routes/dish.routes.ts
@@ -1,15 +1,15 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/dish/create";
+import { deleteDish } from "../controllers/dish/delete";
 import { duplicate } from "../controllers/dish/duplicate";
 import { findAll } from "../controllers/dish/findAll";
 import { findById } from "../controllers/dish/findById";
 import { update } from "../controllers/dish/update";
-import { deleteDish } from "../controllers/dish/delete";
 
 export function dishRoutes(app: FastifyInstance) {
   app.post("/dish", create)
   app.get("/dish", findAll)
-  app.get("/dish/:id", findById)
+  app.get("/dish/:dishId", findById)
   app.put("/dish/:id", update)
   app.post("/dish/:dishId/duplicate", duplicate)
   app.delete("/dish/:id/delete", deleteDish)
