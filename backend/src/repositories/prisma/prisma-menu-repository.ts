2607aabe4d2c1758bd6a07f7bfbb3e0c8d62d05a
DevVitorlevi <levivitor8@gmail.com>
2026-01-24 13:46:02 -0300

    feat(repository): add find all method in prisma menu repository

--- backend/src/repositories/prisma/prisma-menu-repository.ts
+++ backend/src/repositories/prisma/prisma-menu-repository.ts
@@ -1,5 +1,6 @@
+import type { Prisma } from "@/generated/prisma/client";
 import { prisma } from "@/lib/prisma";
-import type { CreateMenuInput } from "../DTOs/menu.dtos";
+import type { CreateMenuInput, FindAllFiltersParams } from "../DTOs/menu.dtos";
 import type { MenuRepository } from "../menu-repository";
 
 export class PrismaMenuRepository implements MenuRepository {
@@ -28,4 +29,46 @@ export class PrismaMenuRepository implements MenuRepository {
 
     return menu
   }
+
+  async findAll(params?: FindAllFiltersParams) {
+    const page = params?.page || 1
+    const limit = params?.limit || 20
+    const limitCapped = Math.min(limit, 100)
+    const skip = (page - 1) * limitCapped
+
+    const where: Prisma.CardapioWhereInput = {}
+
+    if (params?.titulo) {
+      where.titulo = {
+        contains: params.titulo,
+        mode: 'insensitive'
+      }
+    }
+    if (params?.data) {
+      const dataFiltro = new Date(params.data)
+      where.AND = [
+        { checkin: { lte: dataFiltro } },
+        { checkout: { gte: dataFiltro } }
+      ]
+    }
+
+    const [menus, total] = await Promise.all([
+      prisma.cardapio.findMany({
+        where,
+        skip,
+        take: limitCapped
+      }),
+      prisma.cardapio.count({ where })
+    ])
+
+    const totalPages = Math.ceil(total / limitCapped)
+
+    return {
+      menus,
+      total,
+      page,
+      totalPages
+    }
+  }
+
 }
