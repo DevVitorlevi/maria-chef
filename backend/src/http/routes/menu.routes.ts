41ea09758a7aea67d40ea31bc6794133806f2eaf
DevVitorlevi <levivitor8@gmail.com>
2026-01-24 13:47:20 -0300

    feat(routes): add route to find all menus

--- backend/src/http/routes/menu.routes.ts
+++ backend/src/http/routes/menu.routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/menu/create";
+import { findAll } from "../controllers/menu/findAll";
 import { findById } from "../controllers/menu/findById";
 
 export function menuRoutes(app: FastifyInstance) {
   app.post("/cardapio", create)
   app.get("/cardapio/:menuId", findById)
+  app.get("/cardapios", findAll)
 }
