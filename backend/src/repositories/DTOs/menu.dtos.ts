4f46d6c28f0cdc5303640b03eb6447fd79310b2a
DevVitorlevi <levivitor8@gmail.com>
2026-01-24 13:06:04 -0300

    feat(menu): add dtos to find all menu

--- backend/src/repositories/DTOs/menu.dtos.ts
+++ backend/src/repositories/DTOs/menu.dtos.ts
@@ -44,3 +44,28 @@ export interface FindByIdMenuOutput {
     updatedAt: Date
   }
 }
+export interface FindAllFiltersParams {
+  titulo?: string
+  data?: string
+  page?: number
+  limit?: number
+}
+
+export interface FindAllMenusOutput {
+  menus: {
+    id: string
+    titulo: string
+    checkin: Date
+    checkout: Date
+    adultos: number
+    criancas: number
+    restricoes: string[]
+    preferencias: string | null
+    geradoPorIA: boolean
+    createdAt: Date
+    updatedAt: Date
+  }[]
+  total: number
+  page: number
+  totalPages: number
+}
