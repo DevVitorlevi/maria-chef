b38ee9569964905fb1c0bfda84e398aca9000ba2
DevVitorlevi <levivitor8@gmail.com>
2026-01-25 17:48:15 -0300

    feat(repository): add delete method in meal repository

--- backend/src/repositories/meal-repository.ts
+++ backend/src/repositories/meal-repository.ts
@@ -1,6 +1,7 @@
 import type { Refeicao } from "@/generated/prisma/client";
-import type { CreateMealInput } from "./DTOs/meal.dtos";
+import type { CreateMealInput, DeleteMealsParams } from "./DTOs/meal.dtos";
 
 export interface MealRepository {
   create(data: CreateMealInput): Promise<Refeicao>
+  delete(params: DeleteMealsParams): void
 }
