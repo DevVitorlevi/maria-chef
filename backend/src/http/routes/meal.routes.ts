4b83b4165194ec57a59a960cd646c9f915b05871
DevVitorlevi <levivitor8@gmail.com>
2026-01-25 18:26:22 -0300

    feat(routes): add route to delete meals

--- backend/src/http/routes/meal.routes.ts
+++ backend/src/http/routes/meal.routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/meal/create";
+import { deleteMeal } from "../controllers/meal/delete";
 
 export function mealRoutes(app: FastifyInstance) {
   app.post("/cardapio/:menuId/refeicao", create)
+  app.delete("/cardapio/:menuId/refeicao/:id", deleteMeal)
 }
