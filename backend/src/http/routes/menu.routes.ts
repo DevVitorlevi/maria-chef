00c3580aacb35d1ce028fe63ed8b86ede002beae
DevVitorlevi <levivitor8@gmail.com>
2026-01-26 22:03:37 -0300

    feat(routes): add route to update menu

--- backend/src/http/routes/menu.routes.ts
+++ backend/src/http/routes/menu.routes.ts
@@ -2,9 +2,11 @@ import type { FastifyInstance } from "fastify";
 import { create } from "../controllers/menu/create";
 import { findAll } from "../controllers/menu/findAll";
 import { findById } from "../controllers/menu/findById";
+import { update } from "../controllers/menu/update";
 
 export function menuRoutes(app: FastifyInstance) {
   app.post("/cardapio", create)
   app.get("/cardapio/:menuId", findById)
   app.get("/cardapios", findAll)
+  app.put("/cardapio/:id", update)
 }
