786e6f7dfca9f81cd7cc5b0f10c430d498287d3f
DevVitorlevi <levivitor8@gmail.com>
2026-01-28 10:11:14 -0300

    refactor(dish): improve dish use-case

--- backend/src/use-cases/dish/delete.ts
+++ backend/src/use-cases/dish/delete.ts
@@ -1,15 +1,14 @@
 import type { DishRepository } from "@/repositories/dish-repository";
+import type { DeleteDishParams } from "@/repositories/DTOs/dish.dtos";
 import { ResourceNotFoundError } from "@/utils/errors/resource-not-found-error";
-
-interface DeleteDishUseCaseRequest {
-  id: string
-}
 export class DeleteDishUseCase {
   constructor(private dishRepository: DishRepository) { }
-  async execute({ id }: DeleteDishUseCaseRequest) {
+
+  async execute({ id }: DeleteDishParams) {
 
     try {
       await this.dishRepository.delete(id)
+
     } catch {
       throw new ResourceNotFoundError()
     }
