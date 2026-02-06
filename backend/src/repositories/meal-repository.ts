3a14008bd937f8aa970f356eb177b2c104c6bfa7
DevVitorlevi <levivitor8@gmail.com>
2026-02-05 23:12:27 -0300

    feat(repository): add meal method in meal repository

--- backend/src/repositories/meal-repository.ts
+++ backend/src/repositories/meal-repository.ts
@@ -1,8 +1,9 @@
 import type { Refeicao } from "@/generated/prisma/client";
-import type { CreateMealInput, DeleteMealsParams, UpdateMealInput, UpdateMealParams, UpdateMealOutput } from "./DTOs/meal.dtos";
+import type { CreateMealInput, DeleteMealsParams, FindByIdMealOutput, FindByIdMealParams, UpdateMealInput, UpdateMealOutput, UpdateMealParams } from "./DTOs/meal.dtos";
 
 export interface MealRepository {
   create(data: CreateMealInput): Promise<Refeicao>
   delete(params: DeleteMealsParams): void
   update(params: UpdateMealParams, data?: UpdateMealInput): Promise<UpdateMealOutput>
+  findById(params: FindByIdMealParams): Promise<FindByIdMealOutput | null>
 }
