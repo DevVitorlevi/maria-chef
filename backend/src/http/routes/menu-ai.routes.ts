91f7d18e2ca57a8d2a2bd0e983954f0582797dd8
DevVitorlevi <levivitor8@gmail.com>
2026-02-05 23:18:10 -0300

    feat(routes): add routes

--- backend/src/http/routes/menu-ai.routes.ts
+++ backend/src/http/routes/menu-ai.routes.ts
@@ -1,6 +1,8 @@
-import type { FastifyInstance } from "fastify";
-import { suggests } from "../controllers/menu-ai/suggests";
+import type { FastifyInstance } from "fastify"
+import { acceptSuggestions } from "../controllers/menu-ai/accept-suggestions"
+import { suggests } from "../controllers/menu-ai/suggests"
 
 export function menuAIRoutes(app: FastifyInstance) {
   app.post("/cardapio/:menuId/suggests", suggests)
+  app.post("/cardapio/:menuId/accept-suggestions", acceptSuggestions)
 }
